6883317746527d314df4de9061d9e923cfdbdd1a
Karen Ramírez Ronquillo <karenramirez.matetm@gmail.com>
2024-05-27 23:30:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+/ ESCRIBIR MIS FUNCIONES
+// FUNCIÓN 1. GENERAR UN COLOR ALEATORIO
+function generaColor(){
+// Generar mi valor de RGB por separado y guardarlos en variables RANDOM 
+const rojo = ;
+const verde = ;
+const azul = ;
+}
+// --> RBG(X, Y, Z)
+
+// FUNCIÓN 2. CAMBIAR EL COLOR DE FONDO DEL CUERPO DE LA PÁGINA
+function cambioBackground(){
+    // generaColor();
+
+}
+
+
+// Buscar el botón en mi HTML por su clase
+// Cada vez que mi botón sea accionado, voy a llamar o ejecutar mi función cambio Background;
